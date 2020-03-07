54d360783ca94d8d9b6328fc0ce9c9ba60c1c55f
brielle <briellemarie28@gmail.com>
2020-03-07 18:57:24 -0500

    added router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectComponent } from './project/project.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+//{path: 'home', component: HomeComponent},
+  {path: 'permits', component: ProjectComponent},
+  {path: 'favorites', component: FavoritesComponent},
+  {path: 'categories', component: CategoriesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
