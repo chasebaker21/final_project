54f1435a10c7271355a90af8f9818f1bdabb658d
Chase Baker <chasebaker@Chases-MacBook-Pro.local>
2020-03-09 14:57:03 -0400

    worked on html/css a lil more, also added side nav

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
+  open: boolean = false;
 
   ngOnInit(): void {
   }
+
+  /* Open when someone clicks on the span element */
+ openNav() {
+   this.open = true;
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+closeNav() {
+  this.open = false;
+}
 
 }
