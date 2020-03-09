573fee1c15fd41739394a256d625d1fac15f1e6a
sam <thesamtastic1@gmail.com>
2020-03-09 16:37:26 -0400

    Successfully queries database with app token

--- src/app/permits-api.service.ts
+++ src/app/permits-api.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PermitsAPIService {
+  private baseURL : string = "https://data.grandrapidsmi.gov/resource/dauu-a4tz.json";
+  private appToken : string = "vCbvNklpbsoW5J7jcoERTiTjt";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  // pulls entire API
+  getPermitsAPI() {
+    const url : string = `${this.baseURL}?$$app_token=${this.appToken}`;
+
+    return this.http.get(url);
+  }
 }
