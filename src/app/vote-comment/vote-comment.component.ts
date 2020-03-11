d487c08c02000c6bd7f487c18191b218427d87d5
Chase Baker <chasebaker@Chases-MacBook-Pro.local>
2020-03-11 16:44:12 -0400

    worked on the votes comments api

--- src/app/vote-comment/vote-comment.component.ts
+++ src/app/vote-comment/vote-comment.component.ts
@@ -11,16 +11,38 @@ export class VoteCommentComponent implements OnInit {
   constructor(private http: HttpClient) { }
   commentForm: boolean = false;
   comment: string;
+  upVoteCount: number = 0;
+  downVoteCount: number = 0;
+  applicationID: string;
 
   ngOnInit(): void {
   }
 
+  // *** WHEN ID IS SET THE COMMENTS AND VOTES "SHOULD" AUTO ASSIGN TO THAT ID *** ???
+
+  // opens and closes the form for leaving name and comment
   commentBtn() {
     this.commentForm = !this.commentForm;
   }
 
-  submit() {
-    this.http.post('http://localhost:5000/comments', { id: 'some-id', comment: this.comment }).subscribe( res => console.log(res) );
+  upVote() {
+    // ****** need to also assign this vote to an application ID *****
+    this.upVoteCount++
+    this.http.post('http://localhost:5000/comments', { id: "this.applicationID", upVote: this.upVoteCount }).subscribe(res => console.log(res));
   }
+
+  downVote() {
+    // ***** need to also assign this vote to an application ID ******
+    this.downVoteCount--
+    this.http.post('http://localhost:5000/comments', { id: "this.applicationID", downVote: this.downVoteCount }).subscribe(res => console.log(res));
+
+  }
+
+
+  // sends the comment and application ID data to server.js
+  submit() {
+    this.http.post('http://localhost:5000/comments', { id: "this.applicationID", comment: this.comment }).subscribe(res => console.log(res));
+  }
+
 
 }
